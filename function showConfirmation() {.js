9a9d55bbeb9797adb219e6cbfc2abcdaff9d3f4d
TheHenta1Man <158206087+TheHenta1Man@users.noreply.github.com>
2024-02-03 13:54:58 +0530

    Add files via upload

---
+++ function showConfirmation() {.js
@@ -0,0 +1,8 @@
+function showConfirmation() {
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "flex";
+    setTimeout(() => {
+        // Add code to open the Spotify playlist or redirect to the playlist URL
+        // For example, window.location.href = 'your_spotify_playlist_url';
+    }, 3000); // Show effects for 3 seconds, then trigger the playlist opening
+}
